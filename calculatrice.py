6e57725703d3560c1b56de11f8f6366c2220006f
teddyh84 <teddyh84@gmail.com>
2024-12-05 14:21:26 +0100

    Add files via upload

---
+++ calculatrice.py
@@ -0,0 +1,28 @@
+from flask import render_template
+
+def calculatrice(request):
+    message_py = ""
+    erreur_py = ""
+    num1_py = ""
+    num2_py = ""
+    result_py = ""
+
+    if request.method == 'POST':
+        if 'effacer' in request.form:
+            message_py = "Tout a été effacé"
+        if 'calculer' in request.form:
+            try:
+                num1_py = int(request.form['num1'])
+                num2_py = int(request.form['num2'])
+                result_py = num1_py + num2_py
+                message_py = "Le résultat est : "
+            except ValueError:
+                erreur_py = "Erreur : veuillez entrer des nombres valides."
+
+    return render_template('calculatrice.html',
+                           message_html=message_py,
+                           erreur_html=erreur_py,
+                           result_html=result_py,
+                           num1_html=num1_py,
+                           num2_html=num2_py
+                           )
---
+++ traduction.py
@@ -0,0 +1,56 @@
+from flask import render_template
+
+def init_traduction(session):
+    session['nb_bonnes_reponses'] = 0
+    session['nb_reponses'] = 0
+    session['mots_anglais'] = ['Apple', 'Ball', 'Cat', 'Dog', 'Fish', 'House', 'Tree', 'Book', 'Chair', 'Table',
+                               'Car', 'Bike', 'Sun', 'Moon', 'Water', 'Fire', 'Food', 'Hand', 'Foot',
+                               'King', 'Queen', 'Boy', 'Girl', 'Baby', 'Happy (Indice : H...)', 'Sad', 'Love', 'Good', 'Bad',
+                               'Day', 'Night', 'Rain', 'Snow', 'Wind', 'Sky', 'Light', 'Dark', 'River', 'Beach',
+                               'Bread', 'Milk', 'Tea', 'Sugar', 'Cake', 'Phone', 'Bag', 'Pen', 'Door', 'Window']
+    session['traduction_fr'] = ['Pomme', 'Balle', 'Chat', 'Chien', 'Poisson', 'Maison', 'Arbre', 'Livre', 'Chaise', 'Table',
+                                'Voiture', 'Vélo', 'Soleil', 'Lune', 'Eau', 'Feu', 'Nourriture', 'Main', 'Pied',
+                                'Roi', 'Reine', 'Garçon', 'Fille', 'Bébé', 'Heureux', 'Triste', 'Amour', 'Bon', 'Mauvais',
+                                'Jour', 'Nuit', 'Pluie', 'Neige', 'Vent', 'Ciel', 'Lumière', 'Sombre', 'Rivière', 'Plage',
+                                'Pain', 'Lait', 'Thé', 'Sucre', 'Gâteau', 'Téléphone', 'Sac', 'Stylo', 'Porte', 'Fenêtre']
+
+def traduction(session, request, random):
+    if 'nb_reponses' not in session:
+        init_traduction(session)
+    message_py = ""
+    erreur_py = ""
+    score_py = ""
+
+    mot_choisi = random.randint(0, len(session['mots_anglais']) - 1)
+    mot_a_afficher = session['mots_anglais'][mot_choisi]
+    mot_a_trouver = session['traduction_fr'][mot_choisi]
+    question_py = mot_a_afficher
+    reponse_attendue_py = mot_a_trouver
+    reponse_py = ""
+
+    if request.method == 'POST':
+        if 'repondre' in request.form:
+            try:
+                reponse_py = request.form['reponse']
+                reponse_attendue_py = request.form['reponse_attendue']
+                if reponse_py.lower() == reponse_attendue_py.lower():
+                    message_py = "Bravo !"
+                    session['nb_bonnes_reponses'] += 1
+                else:
+                    message_py = "La bonne réponse était : " + reponse_attendue_py
+                session['nb_reponses'] += 1
+                score_py = f"Tu as {session['nb_bonnes_reponses']} / {session['nb_reponses']}"
+                del session['mots_anglais'][mot_choisi]
+                del session['traduction_fr'][mot_choisi]
+                reponse_py = ""
+            except:
+                erreur_py = "Erreur : veuillez entrer une réponse valide."
+
+    return render_template('traduction.html',
+                           message_html=message_py,
+                           erreur_html=erreur_py,
+                           score_html=score_py,
+                           question_html=question_py,
+                           reponse_attendue_html=reponse_attendue_py,
+                           reponse_html=reponse_py
+                           )
